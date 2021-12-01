b7ab7a5dd140ea013ebec7f8b6f38063e8603e9e
ceron257 <Ceron257@users.noreply.github.com>
2021-12-01 20:43:47 +0100

    simplified solution

--- day-1.py
+++ day-1.py
@@ -1,7 +1,7 @@
 input = [int(line) for line in open("input-1-1.txt",'r').readlines()]
-increases = [x1 - x0 > 0 for x1, x0 in zip (input[1:], input)].count(True)
+increases = [x1 > x0 for x1, x0 in zip (input[1:], input)].count(True)
 print(increases)
 
 slidingSum = [sum(v) for v in (input[index:index + 3] for index in range(len(input) - 3 + 1))]
-increases2 = [x1 - x0 > 0 for x1, x0 in zip (slidingSum[1:], slidingSum)].count(True)
+increases2 = [x1 > x0 for x1, x0 in zip (slidingSum[1:], slidingSum)].count(True)
 print(increases2)
