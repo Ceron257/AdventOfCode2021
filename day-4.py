384cc5c01e843e07f5a516c16c6e38a63a51c80d
Ceron257 <Ceron257@users.noreply.github.com>
2021-12-04 10:54:15 +0100

    Day 4

---
+++ day-4.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+with open("input-4.txt", "r") as f:
+  input = list(map(int, f.readline().strip().split(',')))
+  gridNumbers = [line.strip() for line in f.readlines() if line.strip() != '']
+
+class Grid:
+  def __init__(self, grid):
+    self.grid = []
+    for line in grid:
+      elements = [l.strip() for l in line.split(' ') if l.strip() != '']
+      self.grid.append(list(map(int, elements)))
+    self.marked = []
+    self.last = None
+  
+  def mark(self, number : int):
+    for l in range(5):
+      if number in self.grid[l]:
+        if(marked := [l, self.grid[l].index(number)]) not in self.marked:
+          self.marked.append(marked)
+          self.last = number
+
+  def won(self):
+    for i in range(5):
+      rows = [j for ii,j in self.marked if ii == i]
+      if len(rows) == 5: return True
+      cols = [ii for ii,j in self.marked if j == i]
+      if len(cols) == 5: return True
+    return False
+
+  def score(self):
+    sum = 0
+    for i in range(5):
+      for j in range(5):
+        if [i,j] in self.marked:
+          continue
+        sum += self.grid[i][j]
+    return sum * self.last
+
+def determineWinner(first):
+  grids = [Grid(gridNumbers[index:index+5]) for index in range(0, len(gridNumbers) - 5 + 1, 5)]
+  winner = None
+  for number in input:
+    for grid in grids:
+      if grid.won(): continue
+      grid.mark(number)
+      if grid.won():
+        if first: return grid
+        winner = grid
+  return winner
+
+print(determineWinner(True).score())
+print(determineWinner(False).score())
