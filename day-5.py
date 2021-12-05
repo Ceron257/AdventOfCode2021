2bd0a175bac8f40f1f81d768a21c029acf72e619
ceron257 <Ceron257@users.noreply.github.com>
2021-12-05 12:22:09 +0100

    Improve solution
    * intodruce normalized method for Point
    * made Point hashable
    * removed unused Line.__str__
    * reduced redundance in Line constructor
    * símplified Line.points
    * simplified countDangerousPoints
    * renamed points to paintPairs

--- day-5.py
+++ day-5.py
@@ -1,50 +1,46 @@
 with open("input-5.txt", "r") as f:
-  points = [line.split('->') for line in f.readlines()]
+  pointPairs = [list(map(lambda v: v.strip(), line.split('->'))) for line in f.readlines()]
 
 X = 0
 Y = 1
 
 class Point:
-  def __init__(self, coordinates):
+  def __init__(self, coordinates : list[int]):
     self.x = coordinates[X]
     self.y = coordinates[Y]
 
+  def normalized(self):
+    x = int(self.x / abs(self.x)) if self.x != 0 else 0
+    y = int(self.y / abs(self.y)) if self.y != 0 else 0
+    return Point([x, y])
+
   def __add__(self, other):
-    return Point ([self.x + other.x, self.y + other.y])
+    return Point([self.x + other.x, self.y + other.y])
 
   def __sub__(self, other):
-    return Point ([self.x - other.x, self.y - other.y])
+    return Point([self.x - other.x, self.y - other.y])
 
   def __eq__(self, other):
     return self.x == other.x and self.y == other.y
 
-  def toList(self):
-    return [self.x, self.y]
+  def __hash__(self):
+    return hash((self.x, self.y))
 
 class Line:
   def __init__(self, points : list[str]):
-    self.p1, self.p2 = [p.split() for p in points]
-    self.p1 = Point(list(map(int, self.p1[0].split(','))))
-    self.p2 = Point(list(map(int, self.p2[0].split(','))))
+    self.p1, self.p2 = list(map(Point, [list(map(int, p.split(','))) for p in points]))
 
-  def __str__(self) -> str:
-      return f"({self.p1.x}, {self.p1.y}) -> ({self.p2.x}, {self.p2.y})"
-  
   def isHorizontal(self):
     return self.p1.y == self.p2.y
 
   def isVertical(self):
     return self.p1.x == self.p2.x
-  
+
   def points(self):
-    dXY = self.p2 - self.p1
-    if dXY.x != 0: dXY.x /= abs(dXY.x)
-    if dXY.y != 0: dXY.y /= abs(dXY.y)
-    dXY.x = int(dXY.x)
-    dXY.y = int(dXY.y)
-    result = []
-    current = self.p1 - dXY
-    while (current := current + dXY) != self.p2 + dXY:
+    dXY = (self.p2 - self.p1).normalized()
+    result = [self.p1, self.p2]
+    current = self.p1
+    while (current := current + dXY) != self.p2:
       result.append(current)
     return result
 
@@ -52,18 +48,12 @@ def countDangerousPoints(lines):
   coveredPoints = {}
   for line in lines:
     for point in line.points():
-      if point.x not in coveredPoints: coveredPoints[point.x] = {}
-      coveredYs = coveredPoints[point.x]
-      if point.y not in coveredYs: coveredYs[point.y] = 0
-      coveredYs[point.y] += 1
+      if point not in coveredPoints: coveredPoints[point] = 0
+      coveredPoints[point] += 1
 
-  dangerousPositions = 0
+  return sum(value > 1 for value in coveredPoints.values())
 
-  for coveredY in coveredPoints:
-    dangerousPositions += sum(value > 1 for value in coveredPoints[coveredY].values())
-  return dangerousPositions
-
-allLines = [Line(line) for line in points]
+allLines = [Line(line) for line in pointPairs]
 filteredLines = [l for line in allLines if (l := line).isHorizontal() or l.isVertical()]
 
 print(countDangerousPoints(filteredLines))
