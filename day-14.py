a7fb50aad11c74a21b2633a9bbf99e7a97cb5d49
ceron257 <Ceron257@users.noreply.github.com>
2021-12-14 23:15:34 +0100

    Simplify solution a bit

--- day-14.py
+++ day-14.py
@@ -7,16 +7,18 @@ with open("input-14.txt", "r") as f:
   f.readline()
   rules = {line[0] : line[1] for l in f.readlines() if (line := l.strip().split(' -> ')) != ''}
 
-def growPolymer(polymerCounts, elementCounts):
-  newCounts = copy.deepcopy(polymerCounts)
+def growPolymer(polymerCounts):
+  newCounts = defaultdict(int)
   for oldPair, oldCount in polymerCounts.items():
-    newCounts[oldPair] -= oldCount
     newCounts[oldPair[0] + rules[oldPair]] += oldCount
     newCounts[rules[oldPair] + oldPair[1]] += oldCount
-    elementCounts[rules[oldPair]] += oldCount
   return newCounts
 
-def printSolution(elementCounts):
+def printSolution(pairCount):
+  elementCounts = {e:0 for e in list(set(rules.values()))}
+  for k,v in pairCount.items():
+      elementCounts[k[0]] += v
+  elementCounts[polymer[-1]] +=1
   mostCommon = max(elementCounts.items(), key=operator.itemgetter(1))[1]
   leastCommon = min(elementCounts.items(), key=operator.itemgetter(1))[1]
   print(mostCommon - leastCommon)
@@ -25,13 +27,9 @@ pairCounts = defaultdict(int)
 for i in range(len(polymer) - 1):
   pairCounts[polymer[i] + polymer[i + 1]] += 1
 
-elementCounts = defaultdict(int)
-for element in set(polymer):
-  elementCounts[element] = polymer.count(element)
-
 for i in range(40):
-  pairCounts = growPolymer(pairCounts, elementCounts)
+  pairCounts = growPolymer(pairCounts)
   if i == 9:
-    printSolution(elementCounts)
+    printSolution(pairCounts)
 
-printSolution(elementCounts)
+printSolution(pairCounts)
